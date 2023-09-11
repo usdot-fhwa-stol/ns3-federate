8b74f655c91771a61a23af6a50db7a08fc96cb0c
Cheng Yuan <cheng.yuan@leidos.com>
2023-09-10 21:48:19 -0400

    Add LTE-V2X node creation

--- src/mosaic-node-manager.cc
+++ src/mosaic-node-manager.cc
@@ -59,15 +59,23 @@ namespace ns3 {
     MosaicNodeManager::MosaicNodeManager() : m_ipAddressHelper("10.1.0.0", "255.255.0.0") {
     }
 
-    void MosaicNodeManager::Configure(MosaicNs3Server* serverPtr) {
+    void MosaicNodeManager::Configure(MosaicNs3Server* serverPtr, CommunicationType commType=1) {
         m_serverPtr = serverPtr;
-        m_wifiChannelHelper.AddPropagationLoss(m_lossModel);
-        m_wifiChannelHelper.SetPropagationDelay(m_delayModel);
-        m_channel = m_wifiChannelHelper.Create();
-        m_wifiPhyHelper.SetChannel(m_channel);
+        if(commType == DSRC){
+            m_wifiChannelHelper.AddPropagationLoss(m_lossModel);
+            m_wifiChannelHelper.SetPropagationDelay(m_delayModel);
+            m_channel = m_wifiChannelHelper.Create();
+            m_wifiPhyHelper.SetChannel(m_channel);
+        } else if (commType == LTE){
+            m_lteHelper = CreateObject<LteHelper>();
+            m_lteV2xHelper = CreateObject<LteV2xHelper>();
+            m_lteV2xHelper->SetLteHelper(m_lteHelper);
+            m_epcHelper = CreateObject<PointToPointEpcHelper>();
+            m_ueSidelinkConfiguration = CreateObject<LteUeRrcSl>();
+        }
     }
 
-    void MosaicNodeManager::CreateMosaicNode(int ID, Vector position) {
+    void MosaicNodeManager::CreateMosaicNode(int ID, Vector position, CommunicationType commType=1) {
         if (m_isDeactivated[ID]) {
             return;
         }
@@ -76,12 +84,61 @@ namespace ns3 {
         NS_LOG_INFO("Created node " << singleNode->GetId());
         m_mosaic2ns3ID[ID] = singleNode->GetId();
 
-        //Install Wave device
-        NS_LOG_INFO("Install WAVE on node " << singleNode->GetId());
-        InternetStackHelper internet;   
-        internet.Install(singleNode);
-        NetDeviceContainer netDevices = m_wifi80211pHelper.Install(m_wifiPhyHelper, m_waveMacHelper, singleNode);
-        m_ipAddressHelper.Assign(netDevices);
+        // Install the appropriate device based on communication type
+        if (commType == DSRC) {
+            NS_LOG_INFO ("Creating wifi helpers for the node...");
+            InternetStackHelper internet;   
+            internet.Install(singleNode);
+            NetDeviceContainer netDevices = m_wifi80211pHelper.Install(m_wifiPhyHelper, m_waveMacHelper, singleNode);
+            m_ipAddressHelper.Assign(netDevices);
+        } else if (commType == LTE) {
+            NS_LOG_INFO ("Creating LTE and V2X helpers for the node...");
+
+            // Install LTE device
+            NetDeviceContainer vehicleDev = m_lteHelper->InstallUeDevice(singleNode);
+
+            // Install IP stack
+            InternetStackHelper internet;
+            internet.Install(singleNode);
+
+            // Assign IP address
+            Ipv4InterfaceContainer vehicleIpIface;
+            vehicleIpIface = m_epcHelper->AssignUeIpv4Address(vehicleDev);
+
+            // Set default gateway
+            Ipv4StaticRoutingHelper Ipv4RoutingHelper;
+            Ptr<Ipv4StaticRouting> vehicleStaticRouting = Ipv4RoutingHelper.GetStaticRouting(singleNode->GetObject<Ipv4>());
+            vehicleStaticRouting->SetDefaultRoute(m_epcHelper->GetUeDefaultGatewayAddress(), 1);
+
+            // Consider buildings for propagation model
+            BuildingsHelper::Install(singleNode);
+
+            // Attach the vehicle to the LTE network
+            m_lteHelper->Attach(vehicleDev);
+
+            // Set up V2X communication on the vehicle
+            Ptr<LteUeNetDevice> ueDevice = DynamicCast<LteUeNetDevice>(singleNode->GetDevice(0));
+            NS_ASSERT(ueDevice != nullptr);
+            Ptr<LteUeRrc> rrc = ueDevice->GetRrc();
+            NS_ASSERT(rrc != nullptr);
+            rrc->SetAttribute("SidelinkEnabled", BooleanValue(true));
+
+            // Activate sidelink bearer for V2X communication
+            // Create a LteSlTft object for traffic flow template
+            Ipv4Address groupIp = Ipv4Address("225.0.0.1");         // TODO: IP address
+            uint32_t groupL2 = 0x01;                                // TODO: MAC layer
+            Ptr<LteSlTft> tft = CreateObject<LteSlTft>(LteSlTft::BIDIRECTIONAL, groupIp, groupL2); 
+            m_lteV2xHelper->ActivateSidelinkBearer(Simulator::Now(), vehicleDev, tft);
+
+            // Configure V2X for the vehicle
+            m_ueSidelinkConfiguration->SetSlEnabled(true);
+            m_ueSidelinkConfiguration->SetV2xEnabled(true);
+            m_lteV2xHelper->InstallSidelinkV2xConfiguration(vehicleDev, m_ueSidelinkConfiguration);
+
+            // Set up the LTE V2X communication
+            m_lteV2xHelper->EnableV2xCommunication(vehicleDev, true);
+
+        }
 
         //Install app
         NS_LOG_INFO("Install MosaicProxyApp application on node " << singleNode->GetId());
@@ -159,7 +216,7 @@ namespace ns3 {
     /**
      * @brief Evaluates configuration message and applies it to the node
      */
-    void MosaicNodeManager::ConfigureNodeRadio(uint32_t nodeId, bool radioTurnedOn, int transmitPower) {
+    void MosaicNodeManager::ConfigureNodeRadio(uint32_t nodeId, bool radioTurnedOn, int transmitPower, CommunicationType commType=1) {
         if (m_isDeactivated[nodeId]) {
             return;
         }
@@ -175,16 +232,20 @@ namespace ns3 {
         if (radioTurnedOn) {
             ssa->Enable();
             if (transmitPower > -1) {
-                Ptr<WifiNetDevice> netDev = DynamicCast<WifiNetDevice> (node->GetDevice(1));
-                if (netDev == nullptr) {
-                    NS_LOG_ERROR("Inconsistency: no matching NetDevice found on node while configuring");
-                    return;
-                }                        
-                Ptr<YansWifiPhy> wavePhy = DynamicCast<YansWifiPhy> (netDev->GetPhy());
-                if (wavePhy != 0) {
-                    double txDBm = 10 * log10((double) transmitPower);
-                    wavePhy->SetTxPowerStart(txDBm);
-                    wavePhy->SetTxPowerEnd(txDBm);
+                if (commType == DSRC) {
+                    Ptr<WifiNetDevice> netDev = DynamicCast<WifiNetDevice> (node->GetDevice(1));
+                    if (netDev == nullptr) {
+                        NS_LOG_ERROR("Inconsistency: no matching NetDevice found on node while configuring");
+                        return;
+                    }                        
+                    Ptr<YansWifiPhy> wavePhy = DynamicCast<YansWifiPhy> (netDev->GetPhy());
+                    if (wavePhy != 0) {
+                        double txDBm = 10 * log10((double) transmitPower);
+                        wavePhy->SetTxPowerStart(txDBm);
+                        wavePhy->SetTxPowerEnd(txDBm);
+                    }
+                } else if (commType == LTE) {
+                    // TODO: Placeholder for LTE-specific power configuration
                 }
             }
         } else {
