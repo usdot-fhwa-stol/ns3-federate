c6e55defe76b7bd8b05b72ca12c8556fbe4ac8ed
Cheng Yuan <cheng.yuan@leidos.com>
2023-12-12 02:02:19 -0500

    Revert "Update mosaic-node-manager.cc"
    This reverts commit 96006a7d259db0cf9bbfa51d1138700fe3ae5cb2.

--- src/mosaic-node-manager.cc
+++ src/mosaic-node-manager.cc
@@ -167,8 +167,8 @@ namespace ns3 {
 
         std::cout << "FEDERATE DEBUG: assign group L2 address" << std::endl;
         m_groupL2Address = 0x00;
-        Ipv4AddressGenerator::Init(Ipv4Address ("255.255.0.0"), Ipv4Mask("10.1.0.0"));
-        m_clientRespondersAddress = Ipv4AddressGenerator::NextAddress (Ipv4Mask ("10.1.0.0"));
+        Ipv4AddressGenerator::Init(Ipv4Address ("255.0.0.0"), Ipv4Mask("255.0.0.0"));
+        m_clientRespondersAddress = Ipv4AddressGenerator::NextAddress (Ipv4Mask ("255.0.0.0"));
 
 
         std::vector<NetDeviceContainer> txGroups = m_lteV2xHelper->AssociateForV2xBroadcast(m_ueDevs, numOfNode); 
@@ -185,7 +185,7 @@ namespace ns3 {
             std::cout << "FEDERATE DEBUG: clientResponderAddress for node " << ueNode->GetId() << " : " << m_clientRespondersAddress << std::endl;
             m_ns3ID2UniqueAddress[ueNode->GetId()] = m_clientRespondersAddress;
             m_groupL2Address++;
-            m_clientRespondersAddress = Ipv4AddressGenerator::NextAddress (Ipv4Mask ("10.1.0.0"));
+            m_clientRespondersAddress = Ipv4AddressGenerator::NextAddress (Ipv4Mask ("255.0.0.0"));
             //Install app
             std::cout << "Install MosaicProxyApp on node " << ueNode->GetId() << std::endl;
             Ptr<MosaicProxyApp> app = CreateObject<MosaicProxyApp>();
