07acc74f97444e502d0e56c656855ae9cfac9f45
Cheng Yuan <cheng.yuan@leidos.com>
2023-12-15 15:04:37 -0500

    Update mosaic-node-manager.cc

--- src/mosaic-node-manager.cc
+++ src/mosaic-node-manager.cc
@@ -66,56 +66,56 @@ namespace ns3 {
     }
 
     void MosaicNodeManager::InitLte(int numOfNode){
-        NodeContainer ueNodes;
-        NodeContainer enbNode;
-        ueNodes.Create(numOfNode);
-        enbNode.Create(1);
+        // NodeContainer ueNodes;
+        // NodeContainer enbNode;
+        // ueNodes.Create(numOfNode);
+        // enbNode.Create(1);
 
-        MobilityHelper uenMobility;
-        uenMobility.SetMobilityModel("ns3::ConstantVelocityMobilityModel");
-        Ptr<ListPositionAllocator> uenPos = CreateObject<ListPositionAllocator>();
+        // MobilityHelper uenMobility;
+        // uenMobility.SetMobilityModel("ns3::ConstantVelocityMobilityModel");
+        // Ptr<ListPositionAllocator> uenPos = CreateObject<ListPositionAllocator>();
 
-        // Set the distant position to (10000, 10000, 0) which is faraway from the scenario
-        uenPos->Add(Vector(10000, 10000, 0));
-        uenMobility.SetPositionAllocator(uenPos);
-        uenMobility.Install(ueNodes);
+        // // Set the distant position to (10000, 10000, 0) which is faraway from the scenario
+        // uenPos->Add(Vector(10000, 10000, 0));
+        // uenMobility.SetPositionAllocator(uenPos);
+        // uenMobility.Install(ueNodes);
         
-        Ptr<PointToPointEpcHelper> epcHelper = CreateObject<PointToPointEpcHelper>();
+        // Ptr<PointToPointEpcHelper> epcHelper = CreateObject<PointToPointEpcHelper>();
 
-        Ptr<LteHelper> lteHelper = CreateObject<LteHelper>();
-        lteHelper->SetEpcHelper(epcHelper);
-        lteHelper->DisableNewEnbPhy(); 
+        // Ptr<LteHelper> lteHelper = CreateObject<LteHelper>();
+        // lteHelper->SetEpcHelper(epcHelper);
+        // lteHelper->DisableNewEnbPhy(); 
 
-        Ptr<LteV2xHelper> lteV2xHelper = CreateObject<LteV2xHelper> ();
-        lteV2xHelper->SetLteHelper (lteHelper); 
+        // Ptr<LteV2xHelper> lteV2xHelper = CreateObject<LteV2xHelper> ();
+        // lteV2xHelper->SetLteHelper (lteHelper); 
 
-        lteHelper->SetEnbAntennaModelType ("ns3::NistParabolic3dAntennaModel");
-
-
-        lteHelper->SetAttribute ("UseSameUlDlPropagationCondition", BooleanValue(true));
-        lteHelper->SetAttribute ("PathlossModel", StringValue ("ns3::CniUrbanmicrocellPropagationLossModel"));
+        // lteHelper->SetEnbAntennaModelType ("ns3::NistParabolic3dAntennaModel");
 
 
-        Ptr<ListPositionAllocator> enbPos = CreateObject<ListPositionAllocator>(); 
-        enbPos->Add(Vector(0,0,0));
-        MobilityHelper enbMobModel;
-        enbMobModel.SetMobilityModel("ns3::ConstantPositionMobilityModel");
-        enbMobModel.SetPositionAllocator(enbPos);
-        enbMobModel.Install(enbNode);
-
-        NetDeviceContainer enbDevs = lteHelper->InstallEnbDevice(enbNode);
+        // lteHelper->SetAttribute ("UseSameUlDlPropagationCondition", BooleanValue(true));
+        // lteHelper->SetAttribute ("PathlossModel", StringValue ("ns3::CniUrbanmicrocellPropagationLossModel"));
 
 
-        // Required to use NIST 3GPP model
-        BuildingsHelper::Install (enbNode);
-        BuildingsHelper::Install (ueNodes);
-        BuildingsHelper::MakeMobilityModelConsistent (); 
+        // Ptr<ListPositionAllocator> enbPos = CreateObject<ListPositionAllocator>(); 
+        // enbPos->Add(Vector(0,0,0));
+        // MobilityHelper enbMobModel;
+        // enbMobModel.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+        // enbMobModel.SetPositionAllocator(enbPos);
+        // enbMobModel.Install(enbNode);
 
-        for (uint16_t i=0; i<ueNodes.GetN();i++)
-        {
-            m_ns3Id2DeviceId[ueNodes.Get(i)->GetId()] = i;
-            m_preDefineNodeIds.push_back(ueNodes.Get(i)->GetId());
-        }
+        // NetDeviceContainer enbDevs = lteHelper->InstallEnbDevice(enbNode);
+
+
+        // // Required to use NIST 3GPP model
+        // BuildingsHelper::Install (enbNode);
+        // BuildingsHelper::Install (ueNodes);
+        // BuildingsHelper::MakeMobilityModelConsistent (); 
+
+        // for (uint16_t i=0; i<ueNodes.GetN();i++)
+        // {
+        //     m_ns3Id2DeviceId[ueNodes.Get(i)->GetId()] = i;
+        //     m_preDefineNodeIds.push_back(ueNodes.Get(i)->GetId());
+        // }
 
         // lteHelper->SetAttribute("UseSidelink", BooleanValue (true));
         // NetDeviceContainer ueRespondersDevs = lteHelper->InstallUeDevice (ueNodes);
