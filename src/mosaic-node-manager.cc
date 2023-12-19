f651ca7577993c9802af6d7e75b2227f98e9be53
Cheng Yuan <cheng.yuan@leidos.com>
2023-12-19 09:16:39 -0500

    Update mosaic-node-manager.cc

--- src/mosaic-node-manager.cc
+++ src/mosaic-node-manager.cc
@@ -234,7 +234,6 @@ namespace ns3 {
         m_lteHelper->EnableTraces();
         AnimationInterface anim("animation.xml");
         for (uint32_t i = 0; i < m_ueNodes.GetN(); ++i) {
-            anim.SetConstantPosition(m_ueNodes.Get(i), x, y); // Set position if not using a mobility model
             anim.UpdateNodeDescription(m_ueNodes.Get(i), "Node " + std::to_string(i)); // Set node descriptions
             anim.UpdateNodeColor(m_ueNodes.Get(i), 255, 0, 0); // Set node color (R, G, B)
         }
