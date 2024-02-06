fa869b69a7f48d21e1878239e1e63a56d7b35e3e
Cheng Yuan <cheng.yuan@leidos.com>
2024-02-06 17:45:48 -0500

    remove log

--- src/mosaic-node-manager.cc
+++ src/mosaic-node-manager.cc
@@ -299,7 +299,6 @@ namespace ns3 {
         if (m_isDeactivated[nodeID]) {
             return;
         }
-        std::cout<<"Packet received, add receive packet to server"<<std::endl;
         m_serverPtr->AddRecvPacket(recvTime, pack, nodeID, msgID);
     }
 
